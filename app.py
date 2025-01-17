0641d72a22ab885bd02aa725f41c137987f78fc7
Anuj Koya <162292409+kanujlol@users.noreply.github.com>
2025-01-17 18:54:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, jsonify, render_template
+import urllib.parse
 
 app = Flask(__name__)
-
-# Corrected import for URL quoting
-from urllib.parse import quote
 
 # Store the username and password for authentication
 valid_username = "sherlock"
@@ -19,7 +17,7 @@ def login():
     password = request.form['password']
 
     if username == valid_username and password == valid_password:
-        return jsonify({"message": "Success", "flag": "FLAG{this_is_the_flag}"})
+        return jsonify({"message": "Success", "flag": "IET{$$MB}"})
     else:
         return jsonify({"message": "Invalid credentials"}), 401
 
